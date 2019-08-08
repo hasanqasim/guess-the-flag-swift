b912888a537a1e0762d8f3582c168665fd4a8c31
hasanqasim <hasanqasim92@gmail.com>
2019-08-08 23:19:51 +1000

    create guess the flag game

---
+++ Project2/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Muhammad Hasan on 5/8/19.
+//  Copyright © 2019 Muhammad Hasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries: [String] = []
+    var correctAnswer = 0
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        loadCountries()
+        askQuestion(action: nil)
+    }
+    
+    func loadCountries() {
+        countries.append("estonia")
+        countries.append("france")
+        countries.append("germany")
+        countries.append("ireland")
+        countries.append("italy")
+        countries.append("monaco")
+        countries.append("nigeria")
+        countries.append("poland")
+        countries.append("russia")
+        countries.append("spain")
+        countries.append("uk")
+        countries.append("us")
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        correctAnswer = Int.random(in: 0...2)
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        countries.shuffle()
+        var message: String
+        if countries[sender.tag] == countries[correctAnswer] {
+            message = "Correct"
+            score += 1
+        } else {
+            message = "Wrong"
+            score -= 1
+        }
+        let alertController = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
+
+}
+
